7beb9534917af9a7b4f19e81a917526cff8f2d75
Rasmus Hansen <rashansen69@gmail.com>
2022-03-23 14:29:45 +0100

    Config change ready for heroku

--- firebase/firebaseConfig.js
+++ firebase/firebaseConfig.js
@@ -1,6 +1,6 @@
 var admin = require("firebase-admin");
 
-var serviceAccount = require("./programmeringseksamen-firebase-adminsdk-6mfha-386eac00e2.json");
+var serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
